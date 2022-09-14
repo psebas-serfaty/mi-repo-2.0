6665f25fcd5f5ad3b39861a5f09a74b887e6fb5d
Pablo Sebastian Serfaty <psebaspier@hotmail.com>
2022-09-13 21:42:50 -0300

    upgrade

---
+++ src/Imprimeinador.java
@@ -0,0 +1,7 @@
+public class Imprimeinador {
+    
+    public void printearTexto(String texto){
+        System.out.println(texto);
+    }
+
+}
---
+++ src/Tanque.java
@@ -0,0 +1,21 @@
+public class Tanque extends Veiculos{
+    Soldado conductor;
+
+    public Tanque(String nombre, Float daño, Float vida, Soldado conductor) {
+        super(nombre, daño, vida);
+        setConductor(conductor);
+    }
+    
+    Soldado getConductor(){
+        return conductor;
+    }
+    public void setConductor(Soldado conductor){
+        this.conductor = conductor;
+    }
+    
+    @Override
+    public String imprimir(){
+        
+        return "[Tanque]" + super.imprimir() + "[Conductor]:" +this.getConductor().imprimir();
+    }
+}
